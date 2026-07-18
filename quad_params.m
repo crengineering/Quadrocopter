c67b0f489baf347da340315fce2126964b09530e
VibeCoding96 <chris.riedl.96@gmx.de>
2026-07-18 14:22:41 +0200

    moved param in model to m-script; removed duplicated F_grav part in kräfte momente subsystem

--- quad_params.m
+++ quad_params.m
@@ -11,7 +11,7 @@ tau  = 0.05;                % s    Zeitkonstante Motor+ESC
 w_max= 1200;                % rad/s max. Rotordrehzahl
 Cd   = diag([0.10 0.10 0.15]);  % linearer Drag [N/(m/s)]
 h0 = 2;
-om0 = [0; 0; 0];   % Anfangs-Drehrate [p;q;r], Default 0
+
 
 % ---- Mixer (Konvention aus Abschnitt 03) ----
 MIX = [ kT     kT     kT     kT;
@@ -20,6 +20,32 @@ MIX = [ kT     kT     kT     kT;
         kQ    -kQ     kQ    -kQ ];
 
 MIX_inv = inv(MIX);
+
+% controller parameters
+% Drehraten controller; inner control loop
+tau_kP = [0.10;0.10;0.216];
+tau_kI = [0.125;0.125;0.324];
+tau_sat_up = [1.3;1.3;0.08];
+tau_sat_low =[-1.3;-1.3;-0.08];
+
+% Lage controller; middle control loop
+om_kP = [3;3;3];
+
+% Position NED controller
+ned_xy_kP = [1.44, 1.44];
+ned_xy_kD = 1.92;
+ned_z_kP  = 2.7;
+ned_z_kD  = 2.5;
+
+axis_switch = [0 1;
+               -1 0];
+% Integrator init
+phi_dot_to_phi = [0;0;0];
+p_ned_dot_to_p_ned = [0;0;0];
+v_dot_to_v_b = [0;0;0];
+om_dot_to_om = [0; 0; 0];
+pt1_w_cmd_to_w = zeros(4,1);
+
 
 % ---- Trimm: Schwebedrehzahl aus 4*kT*w^2 = m*g ----
 w_hover = sqrt(m*g/(4*kT));           % -> 990.5 rad/s, muss < w_max sein!
